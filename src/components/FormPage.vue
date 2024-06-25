1c04286925f7f2e3c143055e217ae9f13e604c23
PavelGalkin <p.galkin@analyticum.pro>
2024-06-25 14:23:47 +0500

    Dashboard version 1

--- src/components/FormPage.vue
+++ src/components/FormPage.vue
@@ -1,117 +1,168 @@
 <template>
-    <h1>Добавление записи</h1>
-    <div class="form-container">
-      <form @submit.prevent="submitCard">
+  <h1>Добавление записи</h1>
+  <div class="form-container">
+    <form @submit.prevent="submitCard">
+      <div class="form-group">
         <label for="title">Название:</label>
-        <input type="text" id="title" v-model="title">
-        <br>
+        <input
+          type="text"
+          id="title"
+          v-model="title"
+          :maxlength="count1"
+          @input="updateCount1"
+        />
+        <span>Осталось: {{ count1 - title.length }}</span>
+      </div>
+      <div class="form-group">
         <label for="description">Описание:</label>
-        <textarea id="description" v-model="description"></textarea>
-        <br>
+        <textarea
+          id="description"
+          v-model="description"
+          :maxlength="count2"
+          @input="updateCount2"
+        ></textarea>
+        <span>Осталось: {{ count2 - description.length }}</span>
+      </div>
+      <div class="button-group">
         <button type="submit" class="send">Отправить</button>
-        <button type="button" class="cancell" @click="goBack">Отмена</button>
-      </form>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'FormPage',
-    data() {
-      return {
-        title: '',
-        description: '',
-      };
+        <button type="button" class="cancel" @click="goBack">Отмена</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import { v4 as uuidv4 } from 'uuid';
+
+export default {
+  name: 'FormPage',
+  data() {
+    return {
+      title: '',
+      description: '',
+      count1: 25,
+      count2: 75 
+    };
+  },
+  methods: {
+    submitCard() {
+      // Проверка на заполнение формы
+      if (this.title === '' || this.description === '') {
+        alert('Заполните все поля');
+      } else {
+        const newCard = {
+          id: uuidv4(),
+          title: this.title,
+          description: this.description
+        };
+
+        let existingCards = JSON.parse(localStorage.getItem('cards')) || [];
+        existingCards.push(newCard);
+        localStorage.setItem('cards', JSON.stringify(existingCards));
+        this.$router.push('/');
+      }
     },
-    methods: {
-        submitCard() {
-      const newCard = {
-        id: (this.$data.cards && this.$data.cards.length) ? this.$data.cards.length + 1 : 1,
-        title: this.title,
-        description: this.description
-      };
-
-      let existingCards = JSON.parse(localStorage.getItem('cards')) || [];
-
-      existingCards.push(newCard);
-
-      localStorage.setItem('cards', JSON.stringify(existingCards));
-
+    goBack() {
       this.$router.push('/');
-    }
+    },
   }
-  };
-  </script>
-  
-  <style scoped>
-  .form-container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 400px;
-  }
-  
-  form {
-    width: 400px;
-    height: 350px;
-    padding: 20px;
-    margin-top: 50px;
-    background-color: #f4f4f4;
-    border-radius: 5px;
-    display: flex;
-    flex-direction: column;
-    box-shadow: 0 0 10px #6cd670;
-     
-  }
-  
-  h1 {
-    text-align: center;
-    margin-bottom: 20px;
-  }
-  
-  label {
-    font-weight: bold;
-  }
-  
-  input,
-  textarea {
-    padding: 8px;
-    margin-bottom: 10px;
-    border-radius: 5px;
-    font-size: 18px;
-    border: 1px solid #ccc;
-    box-sizing: border-box; 
-  }
-  textarea{
-    height: 150px;
-   
-  }
-  
-  .send {
-    background-color: #6cd670;
-    border: 0;
-    color: white;
-    padding: 10px;
-    border-radius: 5px;
-    cursor: pointer;
-    transition: all 0.3s;
-    
-  }
-  
-  .send:hover {
-    background-color: #5bca5f;
-  }
-  .cancell{
-    margin-top: 30px;
-    background-color: #ca2c2c;
-    border: 0;
-    color: white;
-    padding: 10px;
-    border-radius: 5px;
-    cursor: pointer;
-    transition: all 0.3s;
-  }
-  .cancell:hover {
-    background-color: #a00d0d;
-  }
-  </style>
+};
+</script>
+
+<style scoped>
+.form-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 60vh;
+  padding: 20px;
+}
+
+form {
+  width: 100%;
+  max-width: 500px;
+  padding: 30px;
+  background-color: #ffffff;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+h1 {
+  text-align: center;
+  margin-bottom: 20px;
+  color: #333;
+}
+
+.form-group {
+  margin-bottom: 20px;
+}
+
+label {
+  font-weight: bold;
+  margin-bottom: 5px;
+  display: block;
+}
+
+input,
+textarea {
+  width: 100%;
+  padding: 10px;
+  margin-top: 5px;
+  margin-bottom: 10px;
+  border-radius: 5px;
+  font-size: 16px;
+  border: 1px solid #ccc;
+  box-sizing: border-box;
+  transition: border 0.3s;
+}
+
+input:focus,
+textarea:focus {
+  border: 1px solid #6cd670;
+  outline: none;
+}
+
+textarea {
+  height: 120px;
+  resize: none;
+}
+
+span {
+  font-size: 14px;
+      color: #666;
+}
+
+.button-group {
+  display: flex;
+  justify-content: space-between;
+}
+
+.send,
+.cancel {
+  width: 48%;
+  padding: 10px;
+  border: none;
+  color: white;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s, transform 0.3s;
+}
+
+.send {
+  background-color: #6cd670;
+}
+
+.send:hover {
+  background-color: #5bca5f;
+  transform: scale(1.05);
+}
+
+.cancel {
+  background-color: #ca2c2c;
+}
+
+.cancel:hover {
+  background-color: #a00d0d;
+  transform: scale(1.05);
+}
+</style>
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,152 +1,206 @@
 <template>
-    <div>
-        <div class="container">
-        <router-link to="/form" class="add-link">
-            <button class="add-button">Добавить</button>
-        </router-link>
-        <label class="switch">
-            <input type="checkbox">
-            <span class="slider"></span>
-        </label>
-        <div class="text">
-            Click&drag
-        </div>
-</div>
-      <div
-        class="card-container"
-        @dragover.prevent
-        @drop="handleDrop"
-      >
-      <div
-      v-for="card in filteredCards"
-      :key="card.id"
-      class="card"
-      draggable="true"
-      @dragstart="handleDragStart(card)"
-      @dragover="handleDragOver"
-      @dragend="handleDragEnd"
-    >
-      <h3>{{ card.title }}</h3>
-      <p>{{ card.description }}</p>
-    </div>
+  <div class="main-container">
+    <div class="header">
+      <router-link to="/form" class="add-link">
+        <button class="add-button">Добавить</button>
+      </router-link>
+      <label class="switch">
+        <input type="checkbox" v-model="isDragEnabled">
+        <span class="slider"></span>
+      </label>
+      <div class="text">
+         Click&drag
       </div>
     </div>
-  </template>
-  
-  <script>
-  // значения из json файла
-  import jsonData from '../../data.json';
-  
-  export default {
-    name: "HomePage",
-    data() {
-      return {
-        cards: jsonData
-      };
-    },
-    mounted() {
-      const localCards = JSON.parse(localStorage.getItem("cards"));
-      if (localCards) {
-        this.cards = localCards;
-      }
-    },
-    computed: {
+    <div
+      class="card-container"
+      @dragover.prevent
+      @drop="handleDrop"
+    >
+      <div
+        v-for="card in filteredCards"
+        :key="card.id"
+        class="card"
+        :data-id="card.id"
+        :draggable="isDragEnabled"
+        @dragstart="isDragEnabled ? handleDragStart(card) : null"
+        @dragover="isDragEnabled ? handleDragOver : null"
+        @dragend="isDragEnabled ? handleDragEnd : null"
+      >
+        <div class="card-content">
+          <h3>{{ card.title }}</h3>
+          <p>{{ card.description }}</p>
+        </div>
+        <button class="delete-dashboard" @click="deleteCard(card.id)">Удалить</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import jsonData from '../../data.json';
+
+export default {
+  name: "HomePage",
+  data() {
+    return {
+      cards: jsonData,
+      draggedCard: null,
+      isDragEnabled: true,
+    };
+  },
+  mounted() {
+    const localCards = JSON.parse(localStorage.getItem("cards"));
+    if (localCards) {
+        this.cards = localCards;    
+    } else {
+        this.cards = jsonData;
+        this.saveCardsToLocalStorage(); 
+    }
+},
+  computed: {
     filteredCards() {
-    
       return this.cards.filter(card => card !== null);
     }
   },
-    methods: {
-      handleDragStart(card) {
-        this.draggedCard = card;
-      },
-      handleDragOver(e) {
-        e.preventDefault();
-      },
-      handleDrop(e) {
-        if (!this.draggedCard) {
-          return;
-        }
-        const indexToInsert = Array.from(e.target.parentElement.children).indexOf(e.target);
-        const draggedIndex = this.cards.indexOf(this.draggedCard);
-        this.cards.splice(draggedIndex, 1);
-        this.cards.splice(indexToInsert, 0, this.draggedCard);
+  methods: {
+    handleDragStart(card) {
+      this.draggedCard = card;
+    },
+    handleDragOver(e) {
+      e.preventDefault();
+    },
+    handleDrop(e) {
+      if (!this.draggedCard) {
+        return;
+      }
+      const indexToInsert = Array.from(e.target.parentElement.children).indexOf(e.target);
+      const draggedIndex = this.cards.indexOf(this.draggedCard);
+      this.cards.splice(draggedIndex, 1);
+      this.cards.splice(indexToInsert, 0, this.draggedCard);
+      this.saveCardsToLocalStorage();
+      this.draggedCard = null;
+    },
+    handleDragEnd() {
+      this.saveCardsToLocalStorage();
+    },
+    saveCardsToLocalStorage() {
+      localStorage.setItem("cards", JSON.stringify(this.cards));
+    },
+    deleteCard(cardId) {
+      if (confirm("Вы точно хотите удалить выбранную карточку?")) {
+        this.cards = this.cards.filter(card => card.id !== cardId);
         this.saveCardsToLocalStorage();
-        this.draggedCard = null;
-      },
-      handleDragEnd() {
-        this.saveCardsToLocalStorage();
-      },
-      saveCardsToLocalStorage() {
-        localStorage.setItem("cards", JSON.stringify(this.cards));
       }
     }
-  };
-  </script>
-  
-  <style scoped>
-  .add-button {
-    background-color: #6cd670;
-    border: 0;
-    color: white;
-    width: 100px;
-    height: 40px;
-    border-radius: 5px;
-    cursor: pointer;
-    transition: all 0.3s;
   }
-  
-  .add-button:hover {
-    background-color: #5bca5f;
-  }
-  
-  .card-container {
-    margin-top: 30px;
-    display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    gap: 20px;
-  }
-  
-  .card {
-    background-color: #f4f4f4;
-    padding: 10px;
-    border-radius: 5px;
-    cursor: grab;
-    transition: all 0.3s;
-  }
-  
-  .card:hover {
-    box-shadow: 0 0 5px #6cd670;
-  }
-  .checkbox-container {
-  display: block;
-  position: relative;
-  padding-left: 35px;
-  margin-bottom: 12px;
-  cursor: pointer;
-  font-size: 22px;
+};
+</script>
+
+<style scoped>
+.main-container {
+  max-width: 1800px;
+  margin: 0 auto;
+  padding: 20px;
 }
-input [type='checkbox']{
-  
-  position: absolute;
-  opacity: 0;
-  cursor: pointer;
-}
-.container {
+
+.header {
   display: flex;
   align-items: center;
+  margin-bottom: 20px;
 }
 
 .add-link {
-  text-decoration: none; 
-  margin-right: 10px; 
+  text-decoration: none;
+  margin-right: 15px;
 }
+
+.add-button {
+  background-color: #4caf50;
+  border: none;
+  color: white;
+  padding: 10px 20px;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 16px;
+  transition: background-color 0.3s;
+}
+
+.add-button:hover {
+  background-color: #45a049;
+}
+
+/* контейнр карт */
+.card-container {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 20px;
+  margin-top: 30px;
+}
+
+/* карта */
+.card {
+    position: relative;
+    width: calc(100% - 20px); 
+    height: 120px; 
+    background-color: #ffffff;
+    padding: 10px;
+    border-radius: 5px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    cursor: grab;
+    transition: box-shadow 0.3s;
+}
+.card:hover {
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+}
+
+.card-content {
+  margin-bottom: 10px;
+}
+
+.card h3 {
+  margin: 0 0 10px;
+  font-size: 20px;
+}
+
+.card p {
+  margin: 0;
+  color: #666;
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-line-clamp: 2; 
+  -webkit-box-orient: vertical;
+  word-break: break-word; 
+  font-size: 20px;
+}
+
+
+.delete-dashboard {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  background-color: rgb(248, 30, 30);
+  border: none;
+  color: white;
+  padding: 5px 10px;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 14px;
+  transition: background-color 0.3s;
+}
+
+.delete-dashboard:hover {
+  background-color: rgb(219, 23, 23);
+}
+
 
 .switch {
   position: relative;
   display: inline-block;
   width: 60px;
   height: 34px;
+  margin-left: 30px;
+  
 }
 
 .switch input {
@@ -161,8 +215,7 @@ input [type='checkbox']{
   right: 0;
   bottom: 0;
   background-color: #ccc;
-  -webkit-transition: .4s;
-  transition: .4s;
+  transition: background-color 0.4s;
   border-radius: 34px;
 }
 
@@ -174,26 +227,21 @@ input [type='checkbox']{
   left: 4px;
   bottom: 4px;
   background-color: white;
-  -webkit-transition: .4s;
-  transition: .4s;
+  transition: transform 0.4s;
   border-radius: 50%;
 }
 
 input:checked + .slider {
-  background-color: #6cd670;
-}
-
-input:focus + .slider {
-  box-shadow: 0 0 1px #6cd670;
+  background-color: #4caf50;
 }
 
 input:checked + .slider:before {
-  -webkit-transform: translateX(26px);
-  -ms-transform: translateX(26px);
   transform: translateX(26px);
 }
-.text{
-    font-size: 18px;
-    margin-left: 15px;
+
+.text {
+  font-size: 18px;
+  color: #333;
+  margin-left: 10px;
 }
-  </style>
+</style>
